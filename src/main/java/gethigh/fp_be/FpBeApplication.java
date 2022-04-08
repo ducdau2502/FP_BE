837be92b7f92f65032d4982dc338ffa9f39d78c7
ducdau2502 <ducdau2502@gmail.com>
2022-04-08 09:24:16 +0700

    add init

---
+++ src/main/java/gethigh/fp_be/FpBeApplication.java
@@ -0,0 +1,13 @@
+package gethigh.fp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gethigh/fp_be/FpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package gethigh.fp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
