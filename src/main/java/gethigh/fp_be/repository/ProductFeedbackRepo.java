1ca77ad298604fbca336ca17a79f7f1d15594567
ducdau2502 <ducdau2502@gmail.com>
2022-04-13 15:51:56 +0700

    add customer

--- src/main/java/gethigh/fp_be/repository/ProductFeedbackRepo.java
+++ src/main/java/gethigh/fp_be/repository/ProductFeedbackRepo.java
@@ -1,10 +1,16 @@
 package gethigh.fp_be.repository;
 
+import gethigh.fp_be.model.Product;
 import gethigh.fp_be.model.ProductFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductFeedbackRepo extends JpaRepository<ProductFeedback,Long> {
     Iterable<ProductFeedback> findAllByProductFeedback_Id(long id);
+
+    @Query(nativeQuery = true,value = "SELECT COUNT(product_feedback_id)" +
+            "FROM product_feedback where product_feedback_id = :id")
+    String countFeedbackByIdProduct(Long id);
 }
